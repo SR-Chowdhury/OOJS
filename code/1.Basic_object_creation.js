5163a1d5ec5cd276dc6d3b8af2afd3f029562218
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-02-05 11:20:18 +0600

    Output Added in 1.Basic_object_creation.js

--- code/1.Basic_object_creation.js
+++ code/1.Basic_object_creation.js
@@ -1,9 +1,17 @@
+/**************************
+* Bismillahir Rahmanir Rahim
+* OOJS T-01
+* Basic Object Cretion
+* Author : Shihan Chowdhury
+* Thanks to - Twinkle Cats 
+**************************/
+
 var obj = {};
 var obj1 = new Object;
 // Those obj & obj1 are working same
 
-console.log(obj);
-console.log(obj1);
+console.log(obj); // {}
+console.log(obj1); // {}
 // Those output are same
 
 var personObj = {
@@ -13,27 +21,31 @@ var personObj = {
     print: function() {
         console.log(this.name + ', From: ' + this.country);
     }
-}
+}properties
 console.log(personObj);
+
 personObj.print();
 // here we are accessing personObj method by dot(.) notaion
+
 console.log(personObj['name']);
 // here we are accessing personObj properties by Bracket [] notaion
+
 // N.B. In js single or double qotaion is eligible, but majority are used dot ' ' single qotation
 
 personObj.sex = 'Male';
 console.log('Name: ' + personObj.name + ', Sex: ' + personObj.sex);
-// we can extend any object properties outer from object scope; using (.) notation
-
+// we can extend any object properties outer from object scope; using (.) notation, [] notaion can be used as well
 
 personObj['city'] = 'Sylhet';
-console.log(personObj['city']);
-// we can extend any object properties outer from object scope; using [] notation
+console.log('Name: ' + personObj.name + ', City: ' + personObj.city);
+// Name: Shihan, City: Sylhet
 
-// Now i want to travers all object properties. So we are using for in loop
-for (properties in personObj){
+
+// Now i want to travers all object properties. So we are using 'for in' loop
+for (var properties in personObj){
     console.log(properties + ' : ' + personObj[properties]);
 }
 
 
 // Remember we use personObj[properties] because here . notation will not work
+
