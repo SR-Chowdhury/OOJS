2c956360c3cb7284b8bd0f3cbd2b7295b0e33566
SR-Chowdhury <hackedgdown@gmail.com>
2021-02-07 14:36:38 +0600

    Create 6th file in OOJS

---
+++ code/6.JS_Inheritance_with_Prototype.js
@@ -0,0 +1,7 @@
+/**************************
+* Bismillahir Rahmanir Rahim
+* OOJS T-06
+* JS Inheritance with Prototype
+* Author : Shihan Chowdhury
+* Thanks to - Twinkle Cats 
+**************************/
